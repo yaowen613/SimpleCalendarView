6e7b9efa4c65158da0dfc9bd2aaacc0057374de3
茗香墨语 <yaowen424@126.com>
2015-10-28 13:57:35 +0800

    按照自己的思路，把主要的功能代码完成了

--- app/src/main/java/com/yaowen/calendarview04/TestCalendarView.java
+++ app/src/main/java/com/yaowen/calendarview04/TestCalendarView.java
@@ -1,37 +1,50 @@
 package com.yaowen.calendarview04;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.app.Dialog;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 import android.widget.CalendarView;
-
-import java.text.SimpleDateFormat;
+import android.widget.EditText;
 
 /**
- * Created by HelloWorld on 2015/10/28.
+ * Created by YAOWEN on 2015/10/28.
  */
-public class TestCalendarView extends Activity {
-
+public class TestCalendarView extends Activity implements CalendarView.OnDateChangeListener, View.OnClickListener {
+    private View view;
     private CalendarView calendar;
+    private EditText editText;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
-        calendar= (CalendarView) findViewById(R.id.calendarView);
-        long nowTime=calendar.getDate();
-        SimpleDateFormat sdf=new SimpleDateFormat("yyyy年MM月dd日 HH：mm：ss");
-        String time=sdf.format(nowTime);
-        Log.d("TestCalendarView","time:"+time );
-        calendar.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
-            @Override
-            public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
-                month+=1;
-                Log.d("Test","year:"+year +",month:"+month+",dayOfMonth:"+dayOfMonth);
-            }
-        });
-
+        editText = (EditText) findViewById(R.id.et);
+        editText.setOnClickListener(this);
 
     }
 
+    @Override
+    protected Dialog onCreateDialog(int id) {
+        view = View.inflate(TestCalendarView.this, R.layout.calendarview, null);
+        calendar = (CalendarView) view.findViewById(R.id.calendarview2);
+        calendar.setOnDateChangeListener(this);
+        return new AlertDialog.Builder(TestCalendarView.this)
+                .setTitle("请选择日期")
+                .setIcon(android.R.drawable.ic_dialog_info)
+                .setView(view)
+                .setPositiveButton("确定",null)
+                .create();
+    }
+
+    @Override
+    public void onSelectedDayChange(CalendarView view, int year, int month, int dayOfMonth) {
+        editText.setText(year + "年" +( month+1)+ "月" + dayOfMonth+"日");
+    }
+
+    @Override
+    public void onClick(View v) {
+        onCreateDialog(v.getId()).show();
+    }
 }
